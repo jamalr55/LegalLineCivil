5c0fe2c45d49568248c1eaf69900ba3611bfebd1
Jamal D Richardson <jmlrchrdsn55@gmail.com>
2020-01-17 14:28:19 -0600

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -613,19 +613,7 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 			}
 		}
 		
-		/* - Calender */
-		if( $("#calendar").length ) {
-			$('#calendar').fullCalendar({			
-				header: {
-					left: 'prev',
-					right: 'next',
-					center: 'title'
-				},
-				editable: true,
-				eventLimit: false,
-				contentHeight: 'auto',
-			});
-		}
+		
 		
 		/* - Contact Map* */
 		if($("#map-canvas-footer").length==1){
@@ -636,37 +624,7 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 			initialize( "contact-map-canvas" );
 		}
 		
-		/* - Quick Contact Form* */
-		$( "#btn_submit" ).on( "click", function(event) {
-		  event.preventDefault();
-		  var mydata = $("form").serialize();
-			$.ajax({
-				type: "POST",
-				dataType: "json",
-				url: "contact.php",
-				data: mydata,
-				success: function(data) {
-					if( data["type"] == "error" ){
-						$("#alert-msg").html(data["msg"]);
-						$("#alert-msg").removeClass("alert-msg-success");
-						$("#alert-msg").addClass("alert-msg-failure");
-						$("#alert-msg").show();
-					} else {
-						$("#alert-msg").html(data["msg"]);
-						$("#alert-msg").addClass("alert-msg-success");
-						$("#alert-msg").removeClass("alert-msg-failure");					
-						$("#input_name").val("");
-						$("#input_phone").val("");												
-						$("#input_address").val("");										
-						$("#textarea_message").val("");
-						$("#alert-msg").show();				
-					}			
-				},
-				error: function(xhr, textStatus, errorThrown) {
-					alert(textStatus);
-				}
-			});
-		});
+		
 		
 	});	/* - Document On Ready /- */
 	
